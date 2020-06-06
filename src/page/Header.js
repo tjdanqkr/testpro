c8ece4a3e585b1a7addb2a602398b66191e6d5e5
tjdanqkr <tjdanqkr@gmail.com>
2020-06-07 01:16:09 +0900

    조인까지 완성

--- src/page/Header.js
+++ src/page/Header.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { FcHome } from "react-icons/fc";
 import { AiOutlineLogin } from "react-icons/ai";
-import { FiLogOut } from "react-icons/fi";
+import { FiLogOut, } from "react-icons/fi";
+import { BsFillPersonPlusFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "../css/Header.css";
 import Login from "./Login";
+import Join from "./Join";
 
 function Header() {
   const [loginstate, setLoginstate] = useState(false);
+  const [joinstate, setJoinstate] = useState(false);
   const [header, setHeader] =useState("Header");
+  
   const onclick = () => {
+    if(joinstate){
+      setJoinstate(false);
+    }
     if (loginstate) {
       setLoginstate(false);
       setHeader("Header");
     } else {
       setLoginstate(true);
+      setHeader("onHeader");
+    }
+  };
+  const onclick2 = () => {
+    if(loginstate){
+      setLoginstate(false);
+    }
+    if (joinstate) {
+      setJoinstate(false);
+      setHeader("Header");
+    } else {
+      setJoinstate(true);
       setHeader("onHeader");
     }
   };
@@ -25,19 +44,23 @@ function Header() {
   return (
     <div className={header}>
       <div className="homebutton">
-        <Link to="/Map">
+        <Link to="/">
           <FcHome></FcHome>
         </Link>
         
       </div>
       <div className="actionbutton">
           {window.sessionStorage.getItem("id") === null ? (
-            <AiOutlineLogin onClick={onclick}></AiOutlineLogin>
+            <>
+              <AiOutlineLogin onClick={onclick}></AiOutlineLogin>
+              <BsFillPersonPlusFill onClick={onclick2}></BsFillPersonPlusFill>
+            </>
           ) : (
             <FiLogOut onClick={logout}></FiLogOut>
           )}
         </div>
       {loginstate ? <Login></Login> : <p></p>}
+      {joinstate? <Join></Join>:<p></p>}
     </div>
   );
 }
---
+++ src/page/Join.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+
+import "../css/Login.css";
+
+function Join() {
+  const [idcheck, setIdcheck] = useState(false);
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [repw, setRepw] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [checkmessage, setCheckMessage] = useState("");
+  const [joinmessage, setJoinmessage] = useState("");
+
+  const idch = () => {
+    //setIdcheck(true);
+    const post = {
+      id: id,
+    };
+    fetch("/checkId", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(post),
+    })
+      .then(response => response.text())
+      .then(message => {
+        console.log(message);
+        setCheckMessage(message);
+        setIdcheck(true);
+      });
+  };
+  const OnSubmit = e => {
+    //e.preventdefault();
+    if (pw === repw) {
+      const post = {
+        id: id,
+        pw: pw,
+        name: name,
+        age: age,
+      };
+      fetch("/insert", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(post),
+      })
+        .then(response => response.text())
+        .then(message => {
+          console.log(message);
+        });
+      alert("가입완료");
+      window.location.replace("/");
+    } else {
+      alert("비밀번호가 맞지 않습니다.");
+    }
+  };
+
+  return (
+    <div className="Back">
+      <div className="Join">
+        <form onSubmit={OnSubmit}>
+          <input
+            type="text"
+            name="Id"
+            onChange={e => setId(e.target.value)}
+            placeholder="id"
+          />
+          <input type="button" onClick={idch} value="아이디 중복 검사" />
+          {idcheck === true ? <p>{checkmessage}</p> : <p></p>}
+          <input
+            type="password"
+            name="Pw"
+            onChange={e => setPw(e.target.value)}
+            placeholder="password"
+          />
+          <input
+            type="password"
+            name="rePw"
+            onChange={e => setRepw(e.target.value)}
+            placeholder="REpassword"
+          />
+          <input
+            type="text"
+            name="name"
+            onChange={e => setName(e.target.value)}
+            placeholder="name"
+          />
+          <input
+            type="text"
+            name="age"
+            onChange={e => setAge(e.target.value)}
+            placeholder="age"
+          />
+
+          <input type="submit" value="Join" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Join;
