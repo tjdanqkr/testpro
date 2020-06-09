1aff83ddb5d33373c43e5d62deae300557ee43c7
tjdanqkr <tjdanqkr@gmail.com>
2020-06-09 12:28:58 +0900

    Updates

---
+++ precache-manifest.d861eb06f04993a132385ce1c1793b7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a3be8345813e450356d12c6d444d46",
+    "url": "/testpro/index.html"
+  },
+  {
+    "revision": "905380c13b8b42bb5dbb",
+    "url": "/testpro/static/css/main.9b677746.chunk.css"
+  },
+  {
+    "revision": "a2521f9497353b20cbcd",
+    "url": "/testpro/static/js/2.30cc9ebc.chunk.js"
+  },
+  {
+    "revision": "39d96014a8391b3dcd48294d9bc163d2",
+    "url": "/testpro/static/js/2.30cc9ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905380c13b8b42bb5dbb",
+    "url": "/testpro/static/js/main.2a6b3dc3.chunk.js"
+  },
+  {
+    "revision": "bb8485b1a6562deda5ae",
+    "url": "/testpro/static/js/runtime-main.99f77e30.js"
+  },
+  {
+    "revision": "81986e43f853c82006f63078ffa1bc7b",
+    "url": "/testpro/static/media/dong1.81986e43.geojson"
+  }
+]);
