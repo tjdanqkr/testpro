e5c1c1048aeb2e9ac1ee9c25359e8b58aea0e4d6
tjdanqkr <tjdanqkr@gmail.com>
2020-06-09 12:38:14 +0900

    go

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { connect } from "react-redux";
 function App() {
   return (
     <>
-      <Route exact path="/" component={Main}></Route>
+      <Route exact path="/map" component={Main}></Route>
     </>
   );
 }
